3088bdc01ad9a56aed1b915973805cd6ad6d3bec
Teal Dowd <teal.dowd@medtronic.com>
2016-09-30 04:17:39 +0000

    Adds reading function from seperate file into the main.
    Removes the need to have another file, by adding the functionality
    to the main GUI file, farther streamlining the program.

--- mainGUI/ichnaeaGUI.py
+++ mainGUI/ichnaeaGUI.py
@@ -1,6 +1,8 @@
 import Tkinter as tk
 import tkMessageBox
-import cardReadModule as cardRead
+import RPi.GPIO as GPIO
+import signal
+#import cardReadModule as cardRead
 import sys
 import MySQLdb
 import time
@@ -8,9 +10,7 @@ import readID
 from time import gmtime, strftime
 import datetime
 import urllib2
-
-
-
+import MFRC522
 
 continuousRun = True
 
@@ -19,6 +19,75 @@ def quitProgram():
         print "Exiting"
         root.destroy()
         sys.exit()
+
+def cardRead():
+
+    # Capture SIGINT for cleanup when the script is aborted
+    def end_read(signal,frame):
+        print "Ctrl+C captured, ending read."
+        GPIO.cleanup()
+        sys.exit()
+
+    # Hook the SIGINT
+    signal.signal(signal.SIGINT, end_read)
+
+    # Create an object of the class MFRC522
+    MIFAREReader = MFRC522.MFRC522()
+      # This loop keeps checking for chips. If one is near it will get the UID and authenticate
+
+    # infinite read loop
+    while True:
+        
+        # Scan for cards    
+        (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+         # If a card is found
+        if status == MIFAREReader.MI_OK:
+            print "Card detected"
+
+        # Get the UID of the card
+        (status,uid) = MIFAREReader.MFRC522_Anticoll()
+        # If we have the UID, continue
+        if status == MIFAREReader.MI_OK:
+            var1 = str(uid[0])
+            var2 = str(uid[1])
+            var3 = str(uid[2])
+            var4 = str(uid[3])
+
+            ID = int(var1 + var2 + var3 + var4)
+
+            userName = 'failed db connect'
+            userClockNum = 00000
+            
+            try:
+                # check db for id
+                db = MySQLdb.connect("MUDDJ2-D1","RP","12345678","ichnaeadb")
+
+                cursor = db.cursor()
+
+                userNameSearch = "SELECT username FROM users WHERE rfidnum = '%d'" % (ID)
+                clockNumSearch = "SELECT clocknum FROM users WHERE rfidnum = '%d'" % (ID)
+
+                # does the thing
+                cursor.execute(userNameSearch)
+                # fetch the username as a string
+                userName = str(cursor.fetchone())
+                # cuts the ends by 2 and 3 respectivily to take off random trash
+                userName = userName[2:-3]
+                
+                cursor.execute(clockNumSearch)
+                userClockNum = str(cursor.fetchone())
+                userClockNum = int(userClockNum[1:-3])
+                db.close()
+            except:
+                print "Error finding user"
+                #tkMessageBox.showwarning("header", "\n Check ethernet or if user is registered and try again.\n\nContact Teal")
+
+            if userName:
+
+                # Print writing successful, and ask for project input
+                print "\n%s has been successfully logged in" % (userName)
+                
+            return userName, userClockNum
 
 def saveinfo(ID, project, userName):
 
@@ -142,7 +211,7 @@ def buttonPressed():
     project = 0
 
     # Run program to scan card info to file
-    name, ID = cardRead.readAndSave()
+    name, ID = cardRead()
     userName = 'Hello '
     userName +=name
 
