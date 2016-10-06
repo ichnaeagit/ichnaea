1ce1e72e4cb5116714cc1cf957e96318601e47e0
Teal Dowd <teal.dowd@medtronic.com>
2016-10-06 15:50:37 +0000

    added project lookup functionality

--- ichnaeaGUI.py
+++ ichnaeaGUI.py
@@ -1,8 +1,3 @@
-#!/user/bin/env python
-
-#export PATH="/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/mysql/bin:/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platform-tools:~/usr/lib/jvm/jdk-6/bin
-
-
 import Tkinter as tk
 import tkMessageBox
 import cardReadModule as cardRead
@@ -49,71 +44,71 @@ while continuousRun:
 
         except:
             print("Failed connect to db")
-            tkMessageBox.showwarning("header", "Failed to connect to database.\n\nCheck ethernet cord, or contact Teal")         
-
-        
-        
-
+            tkMessageBox.showwarning("header", "Failed to connect to database.\n\nCheck ethernet cord, or contact Teal")        
         
         root.destroy()
-##        button01.destroy()
-##        button02.destroy()
-##        button03.destroy()
-##        button04.destroy()
-##        button05.destroy()
-##        button06.destroy()
-##        button07.destroy()
-##        button08.destroy()
-##        button09.destroy()
-##        button10.destroy()
-##        nameLabel.destroy()
-##        helpText.destroy()
-        
-               
-
 
     def buttonPressed():
-            print 'Please scan your card'
-            project = int()
-            project = 0
-          
-            
-            
-            # Run program to scan card info to file
-            name, ID = cardRead.readAndSave()
-            
-            userName = tk.StringVar()
-            tempName = 'Hello '
-            tempName +=name        
-            userName.set(tempName)
-            
-            nameLabel =tk.Label(root, textvariable=userName, font=("Helvetica",12))
-            nameLabel.pack(pady=1)
-            helpText = tk.Label(root, text='Please pick a project', fg="blue")
-            helpText.pack(pady=3)
+        print 'Please scan your card'
+        project = int()
+        project = 0
 
-            #LETS ME DELETE LATER
-            #global button01, button02, button03, button04, button05, button06, button07, button08, button09, button10, nameLabel, helpText
-            button01 = tk.Button(root,text='CAPA Projects', bg="orange", command=lambda: saveinfo(ID,1), width = '50', height=3)
-            button01.pack(pady=2)
-            button02 = tk.Button(root,text='Packaging Support', bg="blue", fg='white',command=lambda:saveinfo(ID,2), width = '50',height=3)
+        # Run program to scan card info to file
+        name, ID = cardRead.readAndSave()
+        
+        userName = tk.StringVar()
+        tempName = 'Hello '
+        tempName +=name        
+        userName.set(tempName)
+
+        # Prints names
+        nameLabel =tk.Label(root, textvariable=userName, font=("Helvetica",12))
+        nameLabel.pack()
+        helpText = tk.Label(root, text='Please pick a project', fg="blue")
+        helpText.pack(side="top")
+
+        #Get project names
+        db = MySQLdb.connect("MUDDJ2-D1","RP","12345678","ichnaeadb")
+        cursor = db.cursor()
+        projectSearch = "SELECT * FROM users WHERE clocknum = '%d'" % (ID)
+        try:
+            # does the thing
+            cursor.execute(projectSearch)
+            # fetch the username as a string
+            projects = cursor.fetchone()
+        except:
+            print "Getting projects failed"
+        db.close()
+
+        # Prints whichever projects people have, if they have them
+        button01 = tk.Button(root,text = projects[4], bg="orange", command=lambda: saveinfo(ID,projects[4]), width = '50', height=3)
+        button01.pack(side="top")
+        if projects[5]:
+            button02 = tk.Button(root,text=projects[5], bg="blue", fg='white',command=lambda:saveinfo(ID,projects[5]), width = '50',height=3)
             button02.pack(pady=2)
-            button03 = tk.Button(root,text='Operations Support', bg="cyan", command=lambda: saveinfo(ID,3), width = '50',height=3)
+        if projects[6]:
+            button03 = tk.Button(root,text=projects[6], bg="cyan", command=lambda: saveinfo(ID,projects[6]), width = '50',height=3)
             button03.pack(pady=2)
-            button04 = tk.Button(root,text='MCDR/WCR', bg="Yellow", command=lambda: saveinfo(ID,4), width = '50',height=3)
+        if projects[7]:
+            button04 = tk.Button(root,text=projects[7], bg="Yellow", command=lambda: saveinfo(ID,projects[7]), width = '50',height=3)
             button04.pack(pady=2)
-            button05 = tk.Button(root,text='Cost Savings Projects', bg="red", command=lambda: saveinfo(ID,5), width = '50',height=3)
+        if projects[8]:
+            button05 = tk.Button(root,text=projects[8], bg="red", command=lambda: saveinfo(ID,projects[8]), width = '50',height=3)
             button05.pack(pady=2)
-            button06 = tk.Button(root,text='Product Transfer Support', bg="orange", command=lambda: saveinfo(ID,6), width = '50',height=3)
+        if projects[9]:
+            button06 = tk.Button(root,text=projects[9], bg="orange", command=lambda: saveinfo(ID,projects[9]), width = '50',height=3)
             button06.pack(pady=2)
-            button07 = tk.Button(root,text='Training', bg="blue", command=lambda: saveinfo(ID,7), fg='white', width = '50',height=3)
+        if projects[10]:
+            button07 = tk.Button(root,text=projects[10], bg="blue", command=lambda: saveinfo(ID,projects[10]), fg='white', width = '50',height=3)
             button07.pack(pady=2)
-            button08 = tk.Button(root,text='Research Projects', bg="cyan", command=lambda: saveinfo(ID,8), width = '50',height=3)
+        if projects[11]:
+            button08 = tk.Button(root,text=projects[11], bg="cyan", command=lambda: saveinfo(ID,projects[11]), width = '50',height=3)
             button08.pack(pady=2)
-            button09 = tk.Button(root,text='Meetings', bg="yellow", command=lambda: saveinfo(ID,9), width = '50',height=3)
+        if projects[12]:
+            button09 = tk.Button(root,text=projects[12], bg="yellow", command=lambda: saveinfo(ID,projects[12]), width = '50',height=3)
             button09.pack(pady=2)
-            button10 = tk.Button(root,text='Clock Off', bg="red", command=lambda: saveinfo(ID,10), width = '50',height=3)
-            button10.pack(pady=5)
+        button10 = tk.Button(root,text='Clock Off', bg="red", command=lambda: saveinfo(ID,"Clock Off"), width = '50',height=3)
+        button10.pack(pady=5)
         
 
     class MainApplication(tk.Frame):
@@ -140,14 +135,14 @@ while continuousRun:
             
             feedbackLabel = tk.Label(root, textvariable=helpText, fg="blue")
             feedbackLabel.pack(pady=5)
-
             
             root.after(500, buttonPressed)
 
-
     if __name__ == "__main__":
         root = tk.Tk()
-        MainApplication(root).pack(side="top",fill="both",expand=True)
+        root.title("Ichnaea time tracking")
+        #MainApplication(root).pack(side="top",fill="both",expand=True)
+        display = MainApplication(root)
         root.mainloop()
 
 
