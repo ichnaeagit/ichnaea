e101f902ebdb692d120abee7a198dc4f27a71ba5
Teal Dowd <teal.dowd@medtronic.com>
2016-10-20 17:45:52 +0000

    Successfully reads number of users from database

--- qtdesigner/graphTime.py
+++ qtdesigner/graphTime.py
@@ -1,7 +1,36 @@
+import sys
+import MySQLdb
 
 
-def graph(durr,group):
-    print durr
-    print group
+#scope is how many weeks to collect from (1 = up to 1 week from today)
+#group is the string of the group (e.g. "PE")
+def graph(scope,group):
+
+    try:
+        #connect SQL database
+        db = MySQLdb.connect("MUDDJ2-D1","RP","12345678","ichnaeadb")
+
+        #set cursor
+        cursor = db.cursor()
+
+        #finds number of users
+        cursor.execute("SELECT COUNT(username) FROM users")
+        result = str(cursor.fetchall()) #fetches as a string
+        numOfUsers = int(result[2:-5]) #cuts it down to only a number
+    
+        print numOfUsers
+        db.close()
+            
+    except:
+        db.close()
+        print("Failed to counter users from db")
+        #tkMessageBox.showwarning("header", "Failed to write to database. \n\nContact Teal")
 
 
+    
+    #print durr
+    #print group
+
+if __name__ == '__main__':
+    graph(1,"PE")
+    
