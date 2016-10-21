0471f7ae2138880da5c95b829d0f269605ce9a86
Teal Dowd <teal.dowd@medtronic.com>
2016-10-21 15:52:56 +0000

    Added ability to get all the users, sum their total times, and save to array

--- qtdesigner/graphTime.py
+++ qtdesigner/graphTime.py
@@ -2,7 +2,7 @@ import sys
 import MySQLdb
 import tkMessageBox
 
-
+db = MySQLdb.connect("MUDDJ2-D1","RP","12345678","ichnaeadb")
 
 
 def countUsers():
@@ -26,26 +26,72 @@ def countUsers():
         print("Failed to count users from db")
         sys.exit()
 
-# ---- main ----
-#scope is how many weeks to collect from (1 = up to 1 week from today)
-#groups is the string of the group (e.g. "PE")
+def getGraphInfo(allUsers):
+    graphLogs = []
 
-def graph(scope,groups):
-    numOfUsers = countUsers()
-    print "There are %s users" % (numOfUsers)
+    # for every user in the list of all available users (broken down by group)
+    for group in allUsers:
+
+        # since it is broken up into groups of PE, QE etc, go through each one of 
+        for user in group:
+
+            user = int(user) #makes sure user is just a number
+            
+            try:
+                db = MySQLdb.connect("MUDDJ2-D1","RP","12345678","ichnaeadb")
+                cursor = db.cursor()
+
+                # creates queries, based on clock numbers
+                getDurr = "SELECT logdurr FROM logs WHERE clockNum = '%d'" % (user)
+                getName = "SELECT username FROM users WHERE clockNum = '%d'" % (user)
+
+                # Get the username of the person, for easy graphing
+                cursor.execute(getName)
+                name = str(cursor.fetchall())
+                name = name[3:-5]
+                graphLogs.append(name) # saves username
+
+                # get their summed durration
+                cursor.execute(getDurr)
+                durr = str(cursor.fetchall())
+                durr = durr[2:-5]   #cuts off end shit
+                durr = durr.split() #cuts into bit
+                durr = [s.strip('L,)') for s in durr]   #strips out shit
+                durr = [s.strip('(') for s in durr]     #strips out other shit
+
+                # takes array of durration, sums
+                time = float(0)
+                for t in durr:
+                    try:
+                        time = time + float(t)
+                    except:
+                        time = time
+                print time
+                graphLogs.append(time) #saves clocked time in (hours?)
+
+                db.close()
+       
+            except:
+                db.close()
+                print "error!"
+
+    return graphLogs
+    
+
+def getUsers(groups):
 
     #Saves RFID numbers for each person in the selected groups
-    i=0
-    userSearchRFIDs = []
+
+    users = []
 
     for group in groups:
-        i=i+1
+
         
         try:
             db = MySQLdb.connect("MUDDJ2-D1","RP","12345678","ichnaeadb")
             cursor = db.cursor()
 
-            query = "SELECT rfidnum FROM users WHERE groupname = '%s'" % (group)
+            query = "SELECT clocknum FROM users WHERE groupname = '%s'" % (group)
             cursor.execute(query)
             result = str(cursor.fetchall()) #fetches data
             result = result[1:-1]   #cuts off end shit
@@ -53,9 +99,9 @@ def graph(scope,groups):
             result = [s.strip('L,)') for s in result]   #strips out shit
             result = [s.strip('(') for s in result]     #strips out other shit
 
-            userSearchRFIDs.append(group)
-            userSearchRFIDs.append(result)
-            #userSearchRFIDs[i].append(result)
+            #users.append(group)
+            users.append(result)
+            #users[i].append(result)
             #print result
 
             db.close()
@@ -63,15 +109,29 @@ def graph(scope,groups):
             print "error!"
             db.close()
             sys.exit()
-            
+    # returns 2d array of clock numbers which are within criteria, segregated by group 
+    return users
 
-    
-    print userSearchRFIDs
-        
-    
-    #print durr
-    #print group
+#
+#
+#
+#
+# ---- main ----
+#scope is how many weeks to collect from (1 = up to 1 week from today)
+#groups is the string of the group (e.g. "PE")
+
+def graph(scope,groups):
+    #numOfUsers = countUsers()
+
+    allUsers = getUsers(groups)
+
+    graphInfo = getGraphInfo(allUsers)
+
+
+    print graphInfo
+
 
 if __name__ == '__main__':
     graph(1,["PE","QE"])
+    #graph(1,["PE"])
     
