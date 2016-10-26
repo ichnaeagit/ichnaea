f2ff1f33f1a360dcd952278e22a560c2756eb990
Teal Dowd <teal.dowd@medtronic.com>
2016-10-26 16:42:00 +0000

    Script for rotating touch screen. Should be made to run on startup

---
+++ rotateScreen90.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#script to set correct touchscreen orientation after x start
+#this won't rotate the displayed image, only the touchscreen input
+#to rotate the displayed image add the following to /boot/config.txt
+#"display_rotate=1" to rotate display 90 degrees
+#"display_rotate=3" to rotate display 270 degrees
+
+xinput set-prop 'FT5406 memory based driver' 'Evdev Axes Swap' 1
+
+#Uncomment this for 90 rotation
+xinput --set-prop 'FT5406 memory based driver' 'Evdev Axis Inversion' 0 1
+
+#Uncomment this for 270 rotation
+#xinput --set-prop 'FT5406 memory based driver' 'Evdev Axis Inversion' 1 0
