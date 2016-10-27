711d3b6b3931dad0ecd6f23745d154e95f6141d0
Teal Dowd <teal.dowd@medtronic.com>
2016-10-27 07:36:54 +0000

    Optimized some sizing for smaller screen use

--- mainGUI/ichnaeaGUI.py
+++ mainGUI/ichnaeaGUI.py
@@ -109,14 +109,11 @@ def buttonPressed():
 
     # Only runs rest of code if there is a valid ID optained
     if ID != "None":
-        userName = tk.StringVar()
-        tempName = 'Hello '
-        tempName +=name        
-        userName.set(tempName)
-        userName='Please try again'
+        userName = 'Hello '
+        userName +=name
 
         # Prints names
-        nameLabel =tk.Label(root, textvariable=userName, font=("Helvetica",12))
+        nameLabel =tk.Label(root, text=userName, font=("Helvetica",18))
         nameLabel.grid()
         helpText = tk.Label(root, text='Please pick a project', fg="blue")
         helpText.grid()
@@ -138,34 +135,34 @@ def buttonPressed():
 
 
         # Prints whichever projects people have, if they have them
-        button01 = tk.Button(root,text = projects[4], bg="orange", command=lambda: saveinfo(ID,projects[4]), width = '50', height=3)
-        button01.grid(pady=4)
+        button01 = tk.Button(root,text = projects[4], bg="orange", command=lambda: saveinfo(ID,projects[4]), width = '20', height=1, font = ("Helvetica", 24))
+        button01.grid(pady=2)
         if projects[5]:
-            button02 = tk.Button(root,text=projects[5], bg="blue", fg='white',command=lambda:saveinfo(ID,projects[5]), width = '50',height=3)
-            button02.grid(pady=4)
+            button02 = tk.Button(root,text=projects[5], bg="blue", fg='white',command=lambda:saveinfo(ID,projects[5]), width = '20',height=1, font = ("Helvetica", 24))
+            button02.grid(pady=2)
         if projects[6]:
-            button03 = tk.Button(root,text=projects[6], bg="cyan", command=lambda: saveinfo(ID,projects[6]), width = '50',height=3)
-            button03.grid(pady=4)
+            button03 = tk.Button(root,text=projects[6], bg="cyan", command=lambda: saveinfo(ID,projects[6]), width = '20',height=1, font = ("Helvetica", 24))
+            button03.grid(pady=2)
         if projects[7]:
-            button04 = tk.Button(root,text=projects[7], bg="Yellow", command=lambda: saveinfo(ID,projects[7]), width = '50',height=3)
-            button04.grid(pady=4)
+            button04 = tk.Button(root,text=projects[7], bg="Yellow", command=lambda: saveinfo(ID,projects[7]), width = '20',height=1, font = ("Helvetica", 24))
+            button04.grid(pady=2)
         if projects[8]:
-            button05 = tk.Button(root,text=projects[8], bg="red", command=lambda: saveinfo(ID,projects[8]), width = '50',height=3)
-            button05.grid(pady=4)
+            button05 = tk.Button(root,text=projects[8], bg="red", command=lambda: saveinfo(ID,projects[8]), width = '20',height=1, font = ("Helvetica", 24))
+            button05.grid(pady=2)
         if projects[9]:
-            button06 = tk.Button(root,text=projects[9], bg="orange", command=lambda: saveinfo(ID,projects[9]), width = '50',height=3)
-            button06.grid(pady=4)
+            button06 = tk.Button(root,text=projects[9], bg="orange", command=lambda: saveinfo(ID,projects[9]), width = '20',height=1, font = ("Helvetica", 24))
+            button06.grid(pady=2)
         if projects[10]:
-            button07 = tk.Button(root,text=projects[10], bg="blue", command=lambda: saveinfo(ID,projects[10]), fg='white', width = '50',height=3)
-            button07.grid(pady=4)
+            button07 = tk.Button(root,text=projects[10], bg="blue", command=lambda: saveinfo(ID,projects[10]), fg='white', width = '20',height=1, font = ("Helvetica", 24))
+            button07.grid(pady=2)
         if projects[11]:
-            button08 = tk.Button(root,text=projects[11], bg="cyan", command=lambda: saveinfo(ID,projects[11]), width = '50',height=3)
-            button08.grid(pady=4)
+            button08 = tk.Button(root,text=projects[11], bg="cyan", command=lambda: saveinfo(ID,projects[11]), width = '20',height=1, font = ("Helvetica", 24))
+            button08.grid(pady=2)
         if projects[12]:
-            button09 = tk.Button(root,text=projects[12], bg="yellow", command=lambda: saveinfo(ID,projects[12]), width = '50',height=3)
-            button09.grid(pady=4)
-        button10 = tk.Button(root,text='Clock Off', bg="red", command=lambda: saveinfo(ID,"Clock Off"), width = '50',height=3)
-        button10.grid(pady=4)
+            button09 = tk.Button(root,text=projects[12], bg="yellow", command=lambda: saveinfo(ID,projects[12]), width = '20',height=1, font = ("Helvetica", 24))
+            button09.grid(pady=2)
+        button10 = tk.Button(root,text='Clock Off', bg="red", command=lambda: saveinfo(ID,"Clock Off"), width = '20',height=1, font = ("Helvetica", 24))
+        button10.grid(pady=2)
     else: root.destroy()
 
 while continuousRun:
@@ -178,23 +175,17 @@ while continuousRun:
             #main GUI here
 
 
-            quitButton = tk.Button(root, text='Quit program', bg="red", fg ="black",height=1,width=65,font=("Helvetica",9), command = quitProgram)
-            quitButton.grid()
+            #quitButton = tk.Button(root, text='Quit program', bg="red", fg ="black",height=1,width=65,font=("Helvetica",9), command = quitProgram)
+            #quitButton.grid()
 
-            welcomeText = tk.Label(root, text="Ichnaea time tracking Beta\n V0.0.1", font=("papyrus",12), fg="blue", width=20)
-            welcomeText.grid()
-
-            helpText = tk.StringVar()
-            helpText.set('Swipe until project picker appears')
-            
             #scanButton = tk.Button(root,text='Click here to scan', bg="red", fg="white", height = 3, font=("Helvetica",16), command = buttonPressed)
             #scanButton.pack(padx=5, pady=4)
 
-            scanCard = tk.Label(root, text="Please Scan Card", font=("papyrus",24), fg="red", width=20)
-            scanCard.grid(row=3, column=0,columnspan=2,pady=10)
+            scanCard = tk.Label(root, text="Please Scan Card", font=("papyrus",30), fg="red", width=20)
+            scanCard.grid(pady=10)
             
-            feedbackLabel = tk.Label(root, textvariable=helpText, fg="blue")
-            feedbackLabel.grid()
+            #feedbackLabel = tk.Label(root, textvariable=helpText, fg="blue")
+            #feedbackLabel.grid()
             
             root.after(500, buttonPressed)
 
